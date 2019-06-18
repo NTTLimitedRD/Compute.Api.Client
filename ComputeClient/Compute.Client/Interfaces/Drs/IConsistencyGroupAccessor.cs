c29c05005d66d644f13ff5ec3bd9a6df63579af0
Kevin Chen <Kevin.T.Chen@dimensiondata.com>
2019-06-19 09:49:35 +1000

    CCI - 2467 CG fixed

--- ComputeClient/Compute.Client/Interfaces/Drs/IConsistencyGroupAccessor.cs
+++ ComputeClient/Compute.Client/Interfaces/Drs/IConsistencyGroupAccessor.cs
@@ -34,14 +34,7 @@
         /// </summary>
         /// <param name="id">The Consistency group id.</param>
         /// <returns>The selected <see cref="ConsistencyGroupType"/></returns>
-        Task<ConsistencyGroupType> GetConsistencyGroup(System.Guid id);
-
-        /// <summary>
-        /// The Get Consistency Group Detail method.
-        /// </summary>
-        /// <param name="id">The Consistency group id.</param>
-        /// <returns>The selected <see cref="ConsistencyGroupType"/></returns>
-        Task<ConsistencyGroupDetailType> GetConsistencyGroupDetail(System.Guid id);
+        Task<ConsistencyGroupDetailType> GetConsistencyGroup(System.Guid id);
 
         /// <summary>
         /// The Create Consistency Group
